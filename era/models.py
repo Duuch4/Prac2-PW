bb65b3f3c1b6cec0514bb073348236afdd57ee09
Albert <duchfeina@gmail.com>
2021-06-25 16:38:27 +0200

    añade un campo de usuario al campo de facultad y de carrera

--- era/models.py
+++ era/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Facultad(models.Model):
     id_Facultad = models.AutoField(primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse('era:facultad_details', kwargs={'pk': self.pk})
@@ -18,6 +20,7 @@ class Carrera(models.Model):
     abreviado = models.CharField(max_length=20)
     description = models.CharField(max_length=250)
     Facultad_idFacultad = models.ForeignKey(Facultad, on_delete= models.CASCADE,default="1")
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
 
